fc069bf728a1a945b77d2f85780c59980d27e38f
Rostyslav Miniukov <thresh1337@gmail.com>
2020-11-27 23:50:07 +0200

    Adding model for movies. WIP

---
+++ source/js/model/films.js
@@ -0,0 +1,38 @@
+import Observer from './observer.js';
+
+export default class Films extends Observer {
+  constructor() {
+    super();
+
+    this._films = [];
+  }
+
+  setFilms(updateType, films) {
+    this._films = films.slice();
+
+    this._notify(updateType);
+  }
+
+  getFilms() {
+    return this._films;
+  }
+
+  static adaptToClient(results) {
+    const [film, details, credits, ids] = results;
+
+    return {
+      title: details.name || details.title,
+      originalTitle: details.original_name || details.original_title,
+      genres: details.genres,
+      release: details.first_air_date || details.release_date,
+      plot: details.overview,
+      poster: details.poster_path,
+      rating: details.vote_average,
+      id: details.id,
+      type: film.media_type,
+      crew: credits.crew,
+      imdbId: ids.imdb_id,
+      network: details.networks || null
+    };
+  }
+}
