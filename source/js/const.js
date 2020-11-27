557ffeff6f77a7e5610ab26a7df7705fc3aeef7f
Rostyslav Miniukov <thresh1337@gmail.com>
2020-11-27 16:28:39 +0200

    Adding project constants file. WIP

---
+++ source/js/const.js
@@ -0,0 +1,25 @@
+export const RenderPosition = {
+  AFTERBEGIN: `afterbegin`,
+  AFTEREND: `afterend`,
+  BEFOREBEGIN: `beforebegin`,
+  BEFOREEND: `beforeend`
+};
+
+export const ListType = {
+  SEARCH: {
+    TYPE: `SEARCH`,
+    CLASS: `results`,
+    TITLE: `Search results for`
+  },
+  TRENDING: {
+    TYPE: `RESULTS`,
+    CLASS: `trending`,
+    TITLE: `Currently trending titles`
+  }
+};
+
+export const SEARCH_SETTINGS = {
+  LANGUAGE: `&language=en-US`,
+  PAGE: `&page=1`,
+  ADULT: `&include_adult=false`,
+};
